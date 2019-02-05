785675c00cbca1633d79904d3a7d3fa0b7c4f455
Alec Kerrigan <imor231@gmail.com>
2019-02-05 13:58:15 -0500

    Centrality graph

--- centrality-sim2.py
+++ centrality-sim2.py
@@ -2,8 +2,10 @@ import networkx as nx
 import ndlib.models.ModelConfig as mc
 import ndlib.models.epidemics.SISModel as sis
 import matplotlib.pyplot as plt
+import numpy as np
+from scipy import stats
 
-def full_centrality_experiment(g, sim_len, num_experimnts):
+def full_centrality_experiment(g, sim_len, num_experimnts, infection_rate, recovery_rate):
 	# model selection
 	model = sis.SISModel(g)
 
@@ -11,8 +13,8 @@ def full_centrality_experiment(g, sim_len, num_experimnts):
 
 	# Model Configuration
 	cfg = mc.Configuration()
-	cfg.add_model_parameter('beta', 0.01)
-	cfg.add_model_parameter('lambda', 0.005)
+	cfg.add_model_parameter('beta', infection_rate)
+	cfg.add_model_parameter('lambda', recovery_rate)
 	cfg.add_model_parameter("percentage_infected", 0.05)
 	model.set_initial_status(cfg)
 
@@ -25,24 +27,49 @@ def full_centrality_experiment(g, sim_len, num_experimnts):
 		model.iteration_bunch(sim_len)
 		for x in range(N):
 			infection_hist[x] = infection_hist[x] + model.status[x]
+	
+	infection_hist = np.log(infection_hist)
 
+	degree_centrality = []
+	closeness_centrality = []
 	between_centrality = []
 	eigen_centrality = []
 
+	degree_dict = nx.degree_centrality(g)
+	closeness_dict = nx.closeness_centrality(g)
 	between_dict = nx.betweenness_centrality(g)
 	eigen_dict = nx.eigenvector_centrality(g)
 
 	for x in range(N):
+		degree_centrality.append(degree_dict[x])
+		closeness_centrality.append(closeness_dict[x])
 		between_centrality.append(between_dict[x])
 		eigen_centrality.append(eigen_dict[x])
+	
+	degree_centrality = stats.zscore(degree_centrality)
+	closeness_centrality = stats.zscore(closeness_centrality)
+	between_centrality = stats.zscore(between_centrality)
+	eigen_centrality = stats.zscore(eigen_centrality)
 
-	plt.scatter(x = between_centrality, y = infection_hist)
+	fig = plt.figure()
+	ax1 = fig.add_subplot(111)
+
+	ax1.scatter(x = degree_centrality, y=infection_hist, label='Degree', s=10)
+	ax1.scatter(x = closeness_centrality, y=infection_hist, label='Closeness', s=10)
+	ax1.scatter(x = between_centrality, y = infection_hist, label='Betweeness', s=10)
+	ax1.scatter(x = eigen_centrality, y = infection_hist, label='Eigenvector',s=10)
+	ax1.legend()
+	ax1.set_xlabel('Z-Score of Centrality')
+	ax1.set_ylabel('Total Number of Times Infected (log)')
+	plt.xlim((-4, 4))
 	plt.show()
 
 # Size of network
 N = 1000
-sim_len = 20
-num_experiments = 2000
+sim_len = 100
+num_experiments = 4000
 
 # Network topology
 #g = nx.barabasi_albert_graph(N, 5)
+g = nx.erdos_renyi_graph(N, 0.01)
+full_centrality_experiment(g, sim_len, num_experiments, 0.01, 0.01)
